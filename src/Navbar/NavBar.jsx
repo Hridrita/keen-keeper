271efc446a27fa5446cbf39cbeb84b18e266e85d
Hridrita <moonsaha721@gmail.com>
2026-04-13 23:33:53 +0600

    set up project

---
+++ src/Navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2 className='bg-red-500'>hellllooooooo</h2>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Home/Home";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>
+
+      }
+    ]
+   },
+]);
